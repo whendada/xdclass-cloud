067f439bec661339f367774f4bb6ead9fca77878
wenkj <Dailian1234*+>
2021-07-18 20:55:53 +0800

    feature:sentinel 熔断降级

--- xdclass-order-service/src/main/java/net/xdclass/controller/OrderController.java
+++ xdclass-order-service/src/main/java/net/xdclass/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("api/v1/video_order")
@@ -35,6 +36,20 @@ public class OrderController {
         Integer result = videoService.save(video);
         Map<String, Integer> map = new HashMap<>();
         map.put("row", result);
+        return map;
+    }
+
+    int temp = 0;
+    @GetMapping("list")
+    public Object findList() {
+        ++temp;
+        if (temp%3 == 0) {
+            throw  new RuntimeException();
+        }
+//        TimeUnit.SECONDS.sleep(3);
+        Map<String, String> map = new HashMap<>();
+        map.put("title1", "SpringCloud微服务实战");
+        map.put("title2", "xdclass大课");
         return map;
     }
 }
